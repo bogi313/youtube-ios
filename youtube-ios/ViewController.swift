3a2aa4dcfac46ae7903ebcd2031482f4d2476242
Bogdan Ghita <bogdanghita@192-168-0-104.rdsnet.ro>
2020-06-21 14:43:24 +0300

    initial xcode project creation

---
+++ youtube-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-ios
+//
+//  Created by Bogdan Ghita on 21/06/2020.
+//  Copyright © 2020 Bogdan Ghita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
